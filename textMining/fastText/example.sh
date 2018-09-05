026561994539a403e1adca177532b06310d8101f
liuy92 <liuyy92@outlook.com>
2018-09-05 15:16:50 +0800

    Update example.sh

--- textMining/fastText/example.sh
+++ textMining/fastText/example.sh
@@ -2,6 +2,10 @@
 
 cd fastText  #进入安装目录（必须make后才能进行下一步）
 
+#查看输入、输出内容
+
+./fasttext cbow
+
 #构建cbow_model;data.txt的格式为每行为一个样本，每个单词之间用空格分割；输出包含两部分：cbow_model.bin存储模型基本内容，可以load；cbow_model.vec输出词向量
 
 ./fasttext cbow -input data_path/data.txt -output model_path/cbow_model 
